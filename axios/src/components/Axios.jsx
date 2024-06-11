4315169ce2da3c2583446c92a7efe82c034bcb7b
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-06-11 13:09:08 +0200

    reactjs

---
+++ axios/src/components/Axios.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import axios from 'axios'
+
+function Axios() {
+    async  function cal(){
+    await axios.get("http://localhost:3000/getdata").then((response)=>{
+        console.log(response.data)
+    }).catch((err)=>{
+        console.log(err)
+    })
+    };
+    cal()
+  return (
+    <div>
+      <p>calling data</p>
+    </div>
+  )
+}
+
+export default Axios
