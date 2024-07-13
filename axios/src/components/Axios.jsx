3f8c50671e0ca1c846ddbaa73cfef05db73b97fe
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-07-13 23:44:16 +0200

    reactjs

--- axios/src/components/Axios.jsx
+++ axios/src/components/Axios.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function Axios() {
-    async  function cal(){
-    await axios.get("http://localhost:3000/getlocation").then((response)=>{
-        console.log(response.data)
-    }).catch((err)=>{
-        console.log(err)
-    })
-    };
-    cal()
+const Axios = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/getSubject")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(`there was an error fetching the data , ${error}`);
+      });
+  }, []);
   return (
     <div>
-      <p>calling data</p>
+      {data ? (
+        <div>
+          <h1>data from the backend : </h1>
+          <ul>
+            {data.map((item) => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+            {data.map((item) => (
+              <li key={item.id}>{item.subject}</li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>loading...</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Axios
+export default Axios;
