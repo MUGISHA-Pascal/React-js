2b2bbb4920cb4289270f4375e38ff5ef7a1ae235
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-07-13 22:53:52 +0200

    reactjs

--- formhandling/src/components/Form1.js
+++ formhandling/src/components/Form1.js
@@ -1,37 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Form1 = () => {
-    const [data, setinput] = useState({
-        name: "",
-        age: 0
-    })
-    function handleinput(event) {
-        setinput({ ...data, name: event.target.value })
-    }
-    function handleinput2(event) {
-        setinput({ ...data, age: event.target.value })
-    }
+  const [data, setinput] = useState({
+    name: "",
+    age: 0,
+  });
+  function handleinput(event) {
+    setinput({ ...data, name: event.target.value });
+  }
+  function handleinput2(event) {
+    setinput({ ...data, age: event.target.value });
+  }
 
-    function handlesubmit(event, form) {
-        event.preventDefault()
-        form = {
-            data
-        }
-        for (const value in form.data) {
-            console.log(form.data[value])
-        }
+  function handlesubmit(event, form) {
+    event.preventDefault();
+    form = {
+      data,
+    };
+    for (const value in form.data) {
+      console.log(form.data[value]);
     }
-    return (
-        <div>
-            <p>enter your name:</p>
-            <input type="text" onChange={handleinput} />
-            <input type="number" onChange={handleinput2} />
-            <input type="submit" onClick={handlesubmit} />
-            
-            {(data.age == 0) ? <h1></h1> : <h1>{data.age}</h1>}
-            <h1>{data.name}</h1>
-        </div>
-    )
-}
+  }
+  return (
+    <div>
+      <p>enter your name:</p>
+      <input type="text" onChange={handleinput} />
+      <input type="number" onChange={handleinput2} />
+      <input type="submit" onClick={handlesubmit} />
 
-export default Form1
+      {data.age === 0 ? <h1></h1> : <h1>{data.age}</h1>}
+      <h1>{data.name}</h1>
+    </div>
+  );
+};
+
+export default Form1;
