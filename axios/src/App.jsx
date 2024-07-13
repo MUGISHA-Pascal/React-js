8d2f92df26e2ad39ba5be52992f6bfb4a1e37349
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-07-14 00:07:46 +0200

    reactjs

--- axios/src/App.jsx
+++ axios/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import Axios from './components/Axios'
+import "./App.css";
+import Axios2 from "./components/Axios2";
 
 function App() {
-
   return (
     <>
-<Axios/>
+      <Axios2 />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
